fc00aa5374c93c4f11a04192d501daf03b14759b
Palakrudani11 <rudanipalak@gmail.com>
2024-10-19 20:39:01 +0530

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-   //Include constants.php for SITEURL
-   include('../config/constants.php'); 
-   //1. Destory the Session
-   session_destroy(); //Unset $_SESSION['user']
-
-   //2. Redirect to Login Page
-   header('location:'.SITEURL.'admin/login.php');
-?>
